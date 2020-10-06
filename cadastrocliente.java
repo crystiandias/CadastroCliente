9b8b44cc88ed070a9eeffa1187259d62329a43ba
crystiandias <63886754+crystiandias@users.noreply.github.com>
2020-10-05 23:25:35 -0300

    Update cadastrocliente.java

--- cadastrocliente.java
+++ cadastrocliente.java
@@ -5,8 +5,6 @@ public class cadastrocliente {
 
     public static void main(String[] args) {
        UIcadastro formulariocadastro = new UIcadastro();
-       Pessoa cliente=new Pessoa(formulariocadastro.nome, formulariocadastro.cpf,formulariocadastro.sexo,
-               formulariocadastro.endereço,formulariocadastro.cidade,formulariocadastro.estado);
+      formulariocadastro.setVisible(true);
     }
-    
-}
+    }
